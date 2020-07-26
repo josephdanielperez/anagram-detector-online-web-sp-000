68ffab68d6e60c0728533f948ed99423e6b3deee
Joseph Perez <hello@josephdanielperez.com>
2020-07-26 21:00:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,4 +7,14 @@ class Anagram
     @word = word
   end
 
+  def match(array)
+    anagrams = []
+
+    array.each do |word|
+      if word.chars.sort == self.chars.sort
+        anagrams << word
+      end
+      anagrams
+  end
+
 end
